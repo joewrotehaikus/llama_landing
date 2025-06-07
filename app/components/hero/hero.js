a47799506ddc3f62be70c005fde0938b729d33b7
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2025-06-06 20:36:29 -0400

    Quick tweaks to Hero

--- app/components/hero/hero.js
+++ app/components/hero/hero.js
@@ -1,16 +1,25 @@
-import './hero.css'
+import "./hero.css";
+import Image from "next/image";
+import heroimg from "@/public/images/heroimg.svg";
 
 export default function Hero() {
-    return (
-        <div className="main heroblock">
-            <div className="mainsvg">
-                <img src="images/heroimg.svg" alt="Alpaca interface"></img>
-            </div>
-            <div className="info">
-                <h1>Alpaca Now Open!</h1>
-                <p>Turn knowledge into lasting memory. With Alpaca, you can capture notes, organize ideas, and build custom quizzes—all in one place. Whether you're studying, researching, or just learning for fun, Alpaca helps you retain what matters most.</p>
-                <a href="https://alpaca.llamacoop.dev/" target='_blanck'>Try Alpaca</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className="main heroblock">
+      <div className="mainsvg">
+        <Image src={heroimg} alt="Alpaca interface"></Image>
+      </div>
+      <div className="info">
+        <h1>Alpaca Now Open!</h1>
+        <p>
+          Turn knowledge into lasting memory. With Alpaca, you can capture
+          notes, organize ideas, and build custom quizzes—all in one place.
+          Whether you&apos;re studying, researching, or just learning for fun, Alpaca
+          helps you retain what matters most.
+        </p>
+        <a href="https://alpaca.llamacoop.dev/" target="_blanck">
+          Try Alpaca
+        </a>
+      </div>
+    </div>
+  );
 }
