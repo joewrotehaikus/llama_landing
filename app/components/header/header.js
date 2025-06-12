6e069de1d5182322ed17a56441b7cd32e5e8a812
kiztbz <pragyapravita@gmail.com>
2025-06-12 23:04:31 +0530

    Minor changes

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -17,12 +17,13 @@ export default function Header() {
                 <Profile />
             </div>
             <div className="nav-links">
-                <a href="#">Career</a>
+                <a href="#" alt="Career page">Career</a>
                 <a href="#">Projects</a>
                 <a href="#">Membership</a>
                 <a href="#" className="cta-btn">Join Us</a>
             </div>
             <HamburgerMenu />
         </div>
+          
     )
 }
--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import styles from "./page.module.css";
 import Hero from "./components/hero/hero";
 import Perks from "./components/membership_perks/perks";
 import Contribute from "./components/contribute_landing/contribute";
+import About_landing from "./components/about_landing/about_landing";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      <Perks />
+      <About_landing />
+      {/* <Perks /> */}
       <Contribute />
     </div>
   );
