2dfa416a5691b3e0ab0cda57c8ccf8a22628933e
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2025-06-23 23:26:30 -0400

    Fix up layout

--- app/components/hero/hero.js
+++ app/components/hero/hero.js
@@ -1,20 +1,20 @@
 import "./hero.css";
 import Image from "next/image";
-import heroimg from "@/public/images/heroimg.svg";
+import heroimg from "@/public/images/Alpaca_screenshot.jpg";
 
 export default function Hero() {
   return (
     <div className="main heroblock">
       <div className="mainsvg">
-        <Image src={heroimg} alt="Alpaca interface"></Image>
+        <Image src={heroimg} layout="intrinsic" alt="Alpaca interface"></Image>
       </div>
       <div className="info">
         <h1>Alpaca Now Open!</h1>
         <p>
           Turn knowledge into lasting memory. With Alpaca, you can capture
           notes, organize ideas, and build custom quizzes—all in one place.
-          Whether you&apos;re studying, researching, or just learning for fun, Alpaca
-          helps you retain what matters most.
+          Whether you&apos;re studying, researching, or just learning for fun,
+          Alpaca helps you retain what matters most.
         </p>
         <a href="https://alpaca.llamacoop.dev/" target="_blank">
           Try Alpaca
--- app/components/membership_perks/perks.js
+++ app/components/membership_perks/perks.js
@@ -6,8 +6,8 @@ export default function Perks(){
         <h2>Our Services</h2>
         <div className="perkcontainer">
             <div className="perk">Custom app</div>
-            <div className="perk">Tutoring/Mentoring</div>
-            <div className="perk">Web design/Logo design</div>
+            <div className="perk">Tutoring and Mentoring</div>
+            <div className="perk">Web design and Logo design</div>
             <div className="perk">More</div>
         </div>
     </div>
